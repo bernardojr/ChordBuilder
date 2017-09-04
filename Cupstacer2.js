b90f6732e20caa7b91d19b072e867c5b1ea3b278
BernieOdoy004 <cheezeiii365@gmail.com>
2017-09-04 10:23:46 -0400

    Scale.htm added fourths
    Fourths added

---
+++ Cupstacer2.js
@@ -0,0 +1,9 @@
+print("Type some text and press <ENTER>:\t");
+var userInput = readline();
+print("User input: " + userInput);
+    
+    var num1 = userInput / 2;
+    var num2 = userInput * num1;
+    var output = num2 + num1;
+
+document.write(output);
